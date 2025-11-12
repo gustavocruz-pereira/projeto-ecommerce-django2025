710b1c42f502b9b4658ef975a476d0064bd22096
gustavocruz-pereira <guz.cruz.07@gmail.com>
2025-11-12 16:01:08 -0300

    Resolvido secret key

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -12,15 +12,18 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-58b=e_ek-3uu%j1&0=0od)p!m!o&n$)b5tczh%$k_dr+*4r55&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
