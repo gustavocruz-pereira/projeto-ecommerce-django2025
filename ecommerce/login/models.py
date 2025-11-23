9b3ca587e6b6429acb5ccb82a31b49094c02e097
gustavocruz-pereira <guz.cruz.07@gmail.com>
2025-11-23 18:56:28 -0300

    login criado

---
+++ ecommerce/login/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class UserProfile(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password_hash = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
